2e83ab0a8b773a7677eca6c277217bbbf7ad9636
Robert Lawrence <29217166+rtlawrence20@users.noreply.github.com>
2025-09-19 07:14:35 -0600

    impl. method to handle req., determined big O, desciption and thoughts in solution.txt

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,9 +1,21 @@
 function consecutiveSubstrings(string) {
   // type your code here
+  const result = [];
+
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j <= string.length; j++) {
+      result.push(string.slice(i, j));
+    }
+  }
+  return result;
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: ['t', 'tr', 'tri', 'triv', 'trivi', 'trivia', 'trivial',"+
+     "'r', 'ri', 'riv', 'rivi', 'rivia', 'rivial', 'i', 'iv', 'ivi', 'ivia', 'ivial', 'v', 'vi', 'via', 'vial', 'i', 'ia', 'ial', 'a', 'al', 'l']");
+  console.log("=>", consecutiveSubstrings("trivial"));
+
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
